6aafef5caa40fdd8ed24d521b2714be86edd0bed
200382210 <200382210@student.georgianc.on.ca>
2019-05-23 17:05:25 -0400

    5 Another

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send('hey');
+    res.send('hey mot***f***er');
 });
 
 app.get('/about', (req,res) =>{
