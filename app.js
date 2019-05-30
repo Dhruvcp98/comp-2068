1546d638ded9754fe1b9334950fa62ae14417297
200382210 <200382210@student.georgianc.on.ca>
2019-05-30 16:05:18 -0400

    starting class 3

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('hey  mot***f***er');
-});
 
-app.get('/about', (req,res) =>{
-    res.send('i like this');
-});
+//our views path 
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+//our routes
+const routes = require('./routes.js');
+app.use('/', routes);
 
 const port = (process.env.PORT || 4000);
 app.listen(port, () => console.log(`Listening on ${port}`));
+
